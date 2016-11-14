cc9234f0e777ca8a94b40fc796f4e2fe9aa6ddb2
Tommi Mäkitalo <tommi@tntnet.org>
2016-11-14 14:07:01 +0100

    Add some comments to examples

--- demo/exampleAsyncReader.cpp
+++ demo/exampleAsyncReader.cpp
@@ -18,6 +18,7 @@
 
 cxxtools::EventLoop loop;
 
+// handler, which is called when a message is received
 void onMessageReceived(tntpub::DataMessage& message)
 {
     MyMessage msg;
@@ -25,6 +26,7 @@ void onMessageReceived(tntpub::DataMessage& message)
     std::cout << cxxtools::Json(msg).beautify(true);
 }
 
+// handler, which is called when the connection is closed by the server
 void onCloseClient(tntpub::Client&)
 {
     loop.exit();
@@ -34,21 +36,30 @@ int main(int argc, char* argv[])
 {
     try
     {
+        // initialize logging (read `log.properties` or `log.xml`)
         log_init();
 
+        // accept arguments -i for liste ip and -p for port
         cxxtools::Arg<std::string> ip(argc, argv, 'i');
         cxxtools::Arg<unsigned short> port(argc, argv, 'p', 9001);
 
+        // create transport object and client application
         cxxtools::net::TcpStream peer(ip, port);
         peer.attachedDevice()->setSelector(&loop);
         tntpub::Client client(peer);
 
+        // subscribe to topics passed as arguments
         for (int a = 1; a < argc; ++a)
             client.subscribe(argv[a]);
 
+        // we do not flush the output buffer since this is done
+        // asyncronously by the event loop
+
+        // connect signals to slots
         cxxtools::connect(client.messageReceived, onMessageReceived);
         cxxtools::connect(client.closed, onCloseClient);
 
+        // run the event loop
         loop.run();
     }
     catch (const std::exception& e)
--- demo/exampleReader.cpp
+++ demo/exampleReader.cpp
@@ -21,26 +21,34 @@ int main(int argc, char* argv[])
 {
     try
     {
+        // initialize logging (read `log.properties` or `log.xml`)
         log_init();
 
+        // accept arguments -i for liste ip and -p for port
         cxxtools::Arg<std::string> ip(argc, argv, 'i');
         cxxtools::Arg<unsigned short> port(argc, argv, 'p', 9001);
 
+        // create transport object and client application
         cxxtools::net::TcpStream peer(ip, port);
         tntpub::Client client(peer);
 
+        // subscribe to topics passed as arguments
         for (int a = 1; a < argc; ++a)
             client.subscribe(argv[a]);
 
+        // since output is buffered we need to force sending
+        // the subscribe message
         peer.flush();
 
         while (true)
         {
+            // wait for a message and deserialize it
             MyMessage msg;
             client.readMessage().get(msg);
 
             log_info("got message");
 
+            // output in json format
             std::cout << cxxtools::Json(msg).beautify(true);
         }
     }
